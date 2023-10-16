90e1ee1010bdb3cab047b15108cf4ac70cdbef44
MESRAR Mahmoud <mahmoud.mesrar@cegedim.com>
2023-10-16 21:50:03 +0100

    m

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /var/www/audioGuideServer
-
-#navigate into our working directory where we have all our github files
+#!/bin/bash
+# Change to your app directory
 cd /var/www/audioGuideServer
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-#install node modules
-npm install
-
-#start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+# Start the Node.js application using the script defined in package.json
+npm run server
