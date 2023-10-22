b0db16f2be016e6873d31c588b765b1d6c68b040
MESRAR Mahmoud <mahmoud.mesrar@cegedim.com>
2023-10-22 01:05:57 +0100

    remove first inter error

--- app.js
+++ app.js
@@ -58,8 +58,7 @@ app.use(cors(corsOptions));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(bodyParser.json());
-//app.use('/images', express.static(path.join("images")));
-//app.use(bodyParser.urlencoded({extended : false}));
+
 
 app.use((req,res,next) => {
     //console.log(req)
@@ -75,56 +74,10 @@ app.use((req,res,next) => {
     next();
 })
 
-app.post('/test', (req, res, err) => {
-    console.log(req.body)
-    res.json('Successful post')
-  })
 
-var fs = require('fs');
-var music = './audios/mp3/allthat.mp3'; // filepath
-app.get('/', (req, res) => {
 
-var stat = fs.statSync(music);
-range = req.headers.range;
-var readStream;
-// if there is no request about range
-if (range !== undefined) {
-    // remove 'bytes=' and split the string by '-'
-    var parts = range.replace(/bytes=/, "").split("-");
 
-    var partial_start = parts[0];
-    var partial_end = parts[1];
 
-    if ((isNaN(partial_start) && partial_start.length > 1) || (isNaN(partial_end) && partial_end.length > 1)) {
-        return res.sendStatus(500);         
-    }
-    // convert string to integer (start)
-    var start = parseInt(partial_start, 10);
-    // convert string to integer (end)
-    // if partial_end doesn't exist, end equals whole file size - 1
-    var end = partial_end ? parseInt(partial_end, 10) : stat.size - 1;
-    // content length
-    var content_length = (end - start) + 1;
-
-    res.status(206).header({
-        'Content-Type': 'audio/mpeg',
-        'Content-Length': content_length,
-        'Content-Range': "bytes " + start + "-" + end + "/" + stat.size
-    });
-    
-    // Read the stream of starting & ending part
-    readStream = fs.createReadStream(music, {start: start, end: end});
-} else {
-    res.header({
-        'Content-Type': 'audio/mpeg',
-        'Content-Length': stat.size
-    });
-    readStream = fs.createReadStream(music);
-}
-  readStream.pipe(res);
-    console.log(readStream)
-  //res.json('Hello World')
-})
 
 app.use( '/api/circuits', circuitsRoutes);
 app.use('/api/arrets', arretsRoutes);
