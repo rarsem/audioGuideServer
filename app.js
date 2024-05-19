a4949d721c9230691436675d1de4e2534716415d
mmesrar <m_mesrar@etu.enset-media.ac.ma>
2024-05-19 23:58:58 +0100

    check mongodb connection

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cleanupRouter = require('./routes/cleanup')
 
 const app = express();
 const uri = "mongodb+srv://"+process.env.MONGO_ATLAS_USER+":"+process.env.MONGO_ATLAS_PW+"@cluster0.gnkk5o9.mongodb.net/audioGuideDb?retryWrites=true&w=majority";
-
+console.log(uri);
 mongoose.connect(uri,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
